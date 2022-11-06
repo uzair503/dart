6283dc3e9df56eeb51244ee07c287e9230f74607
uzair hussain <ehboy121@gmail.com>
2022-11-06 13:00:50 +0500

    assignment no 3

---
+++ main.dart
@@ -0,0 +1,37 @@
+void main(){
+//   List names = [
+//    "owais" , "bilal" , "ali" , "uzair"
+//   ];
+//   print(names);
+//   List days = [];
+//     days.add("sunday");
+//   days.add("monday");
+//   days.add("tuesday");
+//   days.add("wednesday");
+//   days.add("thursday");
+//   days.add("friday");
+//   days.add("saturday");
+//       print(days);
+//   List friends = [
+//     "ali" ,
+//     "bilal" ,
+//     "hassan" ,
+//     "uzair" ,
+//     "ahmed" ,
+//     "shahzain" ,
+//     "faiz" ,
+    
+//   ];
+// print(friends[0]);
+//   print(friends[4]);
+//   Map info = {
+//     "name" : "uzair" ,
+//     "address" : "Bahadurabad" ,
+//     "age" : 18 ,
+//     "country" : "pakistan"
+    
+//   };
+//   info["country"] = {"australia"};
+//   print(info);
+  
+}
